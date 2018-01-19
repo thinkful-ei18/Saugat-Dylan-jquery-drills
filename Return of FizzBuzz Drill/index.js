26a8c6b72f5d37de0b07317aa2e808b0005ab14c
Dylan <nielsen.dylan@gmail.com>
2018-01-18 17:14:24 -0800

    Refactor with mentor

--- Return of FizzBuzz Drill/index.js
+++ Return of FizzBuzz Drill/index.js
@@ -1,40 +1,73 @@
-'use strict'
+'use strict';
 
-$('form input[type="submit"]').on('click', (event) => {
-  event.preventDefault();
-  const input = $('#number-choice').val();
+// $('form input[type="submit"]').on('click', event => {
+//   event.preventDefault();
+//   const input = $('#number-choice').val();
 
-  for (let i = 0; i <= input; i++) {
-    if(i === 0){
-     $('.js-results').append(`<div class="fizz-buzz-item">
-    <span>0</span>
-   </div>`)
-   
-    }
-    else if (i % 3 === 0 && i % 5 === 0) {
-      $('.js-results').append(`<div class="fizz-buzz-item fizzbuzz">
-      <span>fizzbuzz</span>
-    </div>`);
-    }
-    else if (i % 5 === 0) {
-      $('.js-results').append(`<div class="fizz-buzz-item buzz">
-      <span>buzz</span>
-    </div>`);
-    }
+//   for (let i = 0; i <= input; i++) {
+//     if (i === 0) {
+//       $('.js-results').append(`<div class="fizz-buzz-item">
+//     <span>0</span>
+//    </div>`);
+//     } else if (i % 3 === 0 && i % 5 === 0) {
+//       $('.js-results').append(`<div class="fizz-buzz-item fizzbuzz">
+//       <span>fizzbuzz</span>
+//     </div>`);
+//     } else if (i % 5 === 0) {
+//       $('.js-results').append(`<div class="fizz-buzz-item buzz">
+//       <span>buzz</span>
+//     </div>`);
+//     } else if (i % 3 === 0) {
+//       $('.js-results').append(`<div class="fizz-buzz-item fizz">
+//       <span>fizz</span>
+//     </div>`);
+//     } else {
+//       $('.js-results').append(`<div class="fizz-buzz-item">
+//       <span>${i}</span>
+//     </div>`);
+//     }
+//   }
+// });
 
-    else if (i % 3 === 0) {
-      $('.js-results').append(`<div class="fizz-buzz-item fizz">
-      <span>fizz</span>
-    </div>`);
-    }
-    else{
-      $('.js-results').append(`<div class="fizz-buzz-item">
-      <span>${i}</span>
-    </div>`);
-    }
+// $('form input[type="submit"]').on('click', event => {
+//   event.preventDefault();
+//   $('.js-results').html('');
+//   const input = $('#number-choice').val();
 
+//   for (let i = 0; i <= input; i++) {
+//     let output = i;
+//     if (i !== 0) {
+//       if (i % 15 === 0) {
+//         output = 'fizzbuzz';
+//       } else if (i % 5 === 0) {
+//         output = 'buzz';
+//       } else if (i % 3 === 0) {
+//         output = 'fizz';
+//       }
+//     }
+//     $('.js-results').append(`<div class="fizz-buzz-item">
+//     <span>${output}</span>
+//    </div>`);
+//     if (typeof output === 'string') {
+//       $('.fizz-buzz-item')
+//         .last()
+//         .addClass(output);
+//     }
+//   }
+// });
+
+const fizzBuzzLogic = input => {
+  if (input !== 0) {
+    if (input % 15 === 0) {
+      return 'fizzbuzz';
+    } else if (input % 5 === 0) {
+      return 'buzz';
+    } else if (input % 3 === 0) {
+      return 'fizz';
+    }
   }
-});
+  return input;
+};
 
 $('form input[type="submit"]').on('click', event => {
   event.preventDefault();
@@ -42,22 +75,11 @@ $('form input[type="submit"]').on('click', event => {
   const input = $('#number-choice').val();
 
   for (let i = 0; i <= input; i++) {
-    let output = i;
-    if (i === 0){
-      output = i;
-    }
-    else if (i % 15 === 0) {
-      output = 'fizzbuzz';
-    } else if (i % 5 === 0 ) {
-      output = 'buzz';
-    } else if (i % 3 === 0) {
-      output = 'fizz';
-    }
-    $('.js-results').append(`<div class="fizz-buzz-item">
+    let output = fizzBuzzLogic(i);
+    $('.js-results').append(`<div class="fizz-buzz-item ${
+      typeof output === 'string' ? output : ''
+    }">
     <span>${output}</span>
    </div>`);
-    if (typeof output === 'string') {
-      $('.fizz-buzz-item').last().addClass(output);
-    }
   }
 });
--- shopping-list-app/index.js
+++ shopping-list-app/index.js
@@ -4,30 +4,30 @@
 // check and uncheck items on the list by clicking the "Check" button
 // permanently remove items from the list
 
+function createNewShoppingListElement(input) {
+  return `<li>
+            <span class="shopping-item">${input}</span>
+            <div class="shopping-item-controls">
+            <button class="shopping-item-toggle">
+              <span class="button-label">check</span>
+            </button>
+            <button class="shopping-item-delete">
+              <span class="button-label">delete</span>
+            </button>
+          </div>
+        </li>`;
+}
+
 $('form button[type="submit"]').on('click', event => {
   event.preventDefault();
   const input = $('form input[type="text"]').val();
   $('form input[type="text"]').val('');
-  
-  $('.shopping-list').append(`<li>
-  <span class="shopping-item">${input}</span>
-  <div class="shopping-item-controls">
-    <button class="shopping-item-toggle">
-      <span class="button-label">check</span>
-    </button>
-    <button class="shopping-item-delete">
-      <span class="button-label">delete</span>
-    </button>
-  </div>
-</li>`);
-
+  $('.shopping-list').append(createNewShoppingListElement(input));
 });
 
 $('.shopping-list').on('click','.shopping-item-toggle',event => {
   $(event.currentTarget).closest('li').find('span:first').toggleClass('shopping-item__checked');
- 
   // listItem.toggleClass('.shopping-item__checked');
-
 });
 
 $('.shopping-list').on('click', '.shopping-item-delete', event => {
